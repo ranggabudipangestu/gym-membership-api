6b2a16b5c11b9d300030fa072b164bc3ef57db77
ranggabudipangestu <ranggabudipangestu@gmail.com>
2021-03-23 15:04:19 +0700

    update database

--- database/gym-membership.sql
+++ database/gym-membership.sql
@@ -10,10 +10,96 @@ Target Server Type    : MYSQL
 Target Server Version : 50508
 File Encoding         : 65001
 
-Date: 2021-03-22 14:44:20
+Date: 2021-03-23 15:03:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `location`
+-- ----------------------------
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE `location` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `city` varchar(255) NOT NULL DEFAULT '',
+  `province` varchar(255) NOT NULL DEFAULT '',
+  `country` varchar(255) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '0:inactive, 1:active',
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of location
+-- ----------------------------
+INSERT INTO location VALUES ('1', 'Suhat', 'JL Soekarno Hatta No 10', 'Malang', 'Jawa Timur', '', '1', '2021-03-23 12:19:42', '2021-03-23 12:24:27');
+
+-- ----------------------------
+-- Table structure for `member`
+-- ----------------------------
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `gender` int(255) DEFAULT NULL COMMENT '0: female, 1:male',
+  `date_of_birth` date DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `membership_type` int(11) DEFAULT NULL,
+  `membership_price` double DEFAULT NULL,
+  `joined_date` date DEFAULT NULL,
+  `expired_date` date DEFAULT NULL,
+  `joined_location` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `membership_type`
+-- ----------------------------
+DROP TABLE IF EXISTS `membership_type`;
+CREATE TABLE `membership_type` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of membership_type
+-- ----------------------------
+INSERT INTO membership_type VALUES ('2', 'GYM', '100000', '30', '1', '2021-03-23 14:20:47', '2021-03-23 14:20:47');
+INSERT INTO membership_type VALUES ('3', 'ZUMBA', '150000', '30', '1', '2021-03-23 14:43:57', '2021-03-23 14:43:57');
+
+-- ----------------------------
+-- Table structure for `tax`
+-- ----------------------------
+DROP TABLE IF EXISTS `tax`;
+CREATE TABLE `tax` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `percentage` double DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tax
+-- ----------------------------
+
 -- ----------------------------
 -- Table structure for `trainer`
 -- ----------------------------
@@ -30,7 +116,7 @@ CREATE TABLE `trainer` (
   `created_date` datetime NOT NULL,
   `updated_date` datetime NOT NULL,
   PRIMARY KEY (`id`,`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of trainer
